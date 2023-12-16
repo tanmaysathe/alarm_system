81b76d84a87adba15b09540c185af74d1abdb87b
Tanmay Sathe <tanmaysathe@gmail.com>
2023-12-16 19:16:00 +0530

    state check added for future may be

--- src/events.cpp
+++ src/events.cpp
@@ -1,6 +1,32 @@
 #include <main.h>
 #include <debug_print/debug_print.h>
 
+void alarmSystemStateChanged(JsonDocument &doc)
+{
+    uint8_t id = doc["id"] | 255;
+    uint8_t source = doc["src"] | EVENT_SOURCE_NONE;
+
+    if (id > 0 || source == EVENT_SOURCE_APP)
+        return;
+
+    AlarmSystemStateParser p;
+    p.rawData = doc["state"] | 0;
+
+    switch (p.val.state)
+    {
+    case ALARM_STATE_DISARMED:
+        break;
+    case ALARM_STATE_ARMED:
+        break;
+    case ALARM_STATE_HOMEARMED:
+        break;
+    case ALARM_STATE_INTRUSION:
+        break;
+    case ALARM_STATE_PANIC:
+        break;
+    }
+}
+
 void onEvent(JsonDocument &doc)
 {
     uint32_t state;
@@ -9,8 +35,7 @@ void onEvent(JsonDocument &doc)
     switch (cmd)
     {
     case CMD_SET_STATE:
-        state = doc["state"] | 0;
-        DebugF("State updated from %d = %d\n", doc["src"], state);
+        alarmSystemStateChanged(doc);
         break;
     default:
         break;
